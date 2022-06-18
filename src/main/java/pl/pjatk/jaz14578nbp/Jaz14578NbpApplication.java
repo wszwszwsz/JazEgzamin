769ea056ba5750cc8fd1ff06006b10f2f08d0b19
wszwszwsz <wszwszwsz1990@gmail.com>
2022-06-18 11:53:20 +0200

    First Commit

---
+++ src/main/java/pl/pjatk/jaz14578nbp/Jaz14578NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz14578nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz14578NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaz14578NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/jaz14578nbp/Jaz14578NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz14578nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaz14578NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
